c437afe14549b37a5eb9266a8a2f3978606c1433
Lawal <awwal1999@gmail.com>
2022-07-06 04:11:16 +0100

    add is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - check for lower case characters
+ * @c: The number to be checked
+ *
+ * Return: Always 1 if a is lower and 0 otherwise
+ */
+int _islower(int c)
+{
+    return (c >= 97 && c <= 122);
+}
