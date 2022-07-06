b9be9ede8745d97f72062fc4946ee612f118c3f5
Lawal Akanbi <awwal1999@gmail.com>
2022-07-05 19:42:21 -0700

    print 1o times the alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+include "main.h"
+
+/**
+ * print_alphabet_x10 - 10 times the alphabet, in lowercase, followed by a new line.
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int i, max = 1;
+	
+	while (max <= 10)
+	{		{
+		i = 97;
+
+		while (i <= 122)
+		{
+			_putchar(i);
+			i++;
+		}
+												_putchar('\n');
+												max++;
+											}
+}
+
