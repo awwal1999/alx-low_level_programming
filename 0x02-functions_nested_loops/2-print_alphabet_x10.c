552fd9efdbe954d6b51a97b5ab04ebed16cd3e4d
Lawal <awwal1999@gmail.com>
2022-07-06 04:04:21 +0100

    trailing whitespace

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - 10 times the alphabet, in lowercase, followed by a new line.
+ * print_alphabet_x10 - 10 times the alphabet, in lowercase.
  * Return: Always 0.
  */
 void print_alphabet_x10(void)
 {
 	int i, max = 1;
-	
+
 	while (max <= 10)
 	{
 		i = 97;
-		
+
 		while (i <= 122)
 		{
 			_putchar(i);
@@ -21,4 +21,3 @@ void print_alphabet_x10(void)
 		max++;
 	}
 }
-
