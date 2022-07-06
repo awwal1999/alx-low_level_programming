a2385552dd60f4cfc77d2d9759323f0e281ae953
Lawal Akanbi <awwal1999@gmail.com>
2022-07-05 19:30:05 -0700

    trailing white space

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
+
 /**
  * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
  * Return: Always void.
  */
 void print_alphabet(void)
 {
-
 	int i = 97;
 
 	while (i <= 122)
