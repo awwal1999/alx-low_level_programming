23e5fdfabcee6d62adf049312dd580de927ddf19
Lawal Akanbi <awwal1999@gmail.com>
2022-07-05 19:19:54 -0700

    add alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
+ * Return: Always void.
+ */
+void print_alphabet(void)
+{
+
+	int i = 97;
+	while (i <= 122)
+	{
+		_putchar(i);
+		i++;	
+	}
+
+	_putchar('\n');
+}
